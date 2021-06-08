9586c2025ab2d94ecdc4c5dc61d6322de5bf691d
Frank Viernau <frank.viernau@here.com>
2021-06-08 11:35:57 +0200

    CocoaPodsTest: Turn two test cases into functional ones
    The analyzer has functional tests with expected result (YAML) files for
    each package manager. So, cocoapods should also have that. Turn these
    two tests into functional tests to have that covered, but also to remove
    dependencies on the Podfile class in order to enable refactoring it
    without breaking the tests.
    
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ analyzer/src/funTest/kotlin/managers/CocoaPodsFunTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2021 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.analyzer.managers
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import java.io.File
+
+import org.ossreviewtoolkit.downloader.VersionControlSystem
+import org.ossreviewtoolkit.utils.normalizeVcsUrl
+import org.ossreviewtoolkit.utils.test.DEFAULT_ANALYZER_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.DEFAULT_REPOSITORY_CONFIGURATION
+import org.ossreviewtoolkit.utils.test.USER_DIR
+import org.ossreviewtoolkit.utils.test.patchExpectedResult
+
+private val SYNTHETIC_PROJECTS_DIR = File("src/funTest/assets/projects/synthetic")
+
+class CocoaPodsFunTest : WordSpec({
+    "resolveSingleProject()" should {
+        "determine dependencies from a Podfile.lock without a dependency tree" {
+            val definitionFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods/regular/Podfile.lock").absoluteFile
+            val expectedResult = getExpectedResult(
+                definitionFile = definitionFile,
+                expectedResultFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods-regular-expected-output.yml")
+            )
+
+            val result = createCocoaPods().resolveSingleProject(definitionFile)
+
+            result.toYaml() shouldBe expectedResult
+        }
+
+        "determine dependencies from a Podfile.lock with a dependency tree" {
+            val definitionFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods/dep-tree/Podfile.lock").absoluteFile
+            val expectedResult = getExpectedResult(
+                definitionFile = definitionFile,
+                expectedResultFile = SYNTHETIC_PROJECTS_DIR.resolve("cocoapods-dep-tree-expected-output.yml")
+            )
+
+            val result = createCocoaPods().resolveSingleProject(definitionFile)
+
+            result.toYaml() shouldBe expectedResult
+        }
+    }
+})
+
+private fun getExpectedResult(definitionFile: File, expectedResultFile: File): String {
+    val projectDir = definitionFile.parentFile
+    val vcsDir = VersionControlSystem.forDirectory(projectDir)!!
+    val vcsRevision = vcsDir.getRevision()
+    val vcsUrl = vcsDir.getRemoteUrl()
+    val vcsPath = vcsDir.getPathToRoot(projectDir)
+
+    return patchExpectedResult(
+        expectedResultFile,
+        definitionFilePath = "$vcsPath/${definitionFile.name}",
+        path = vcsPath,
+        revision = vcsRevision,
+        url = normalizeVcsUrl(vcsUrl)
+    )
+}
+
+private fun createCocoaPods(): CocoaPods =
+    CocoaPods.Factory().create(
+        analysisRoot = USER_DIR,
+        analyzerConfig = DEFAULT_ANALYZER_CONFIGURATION,
+        repoConfig = DEFAULT_REPOSITORY_CONFIGURATION
+    )
